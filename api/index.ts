73e18b6a7e6609414f0a34c75d9aff635f53eb7e
Fahad <fahad.45854@iqra.edu.com>
2024-09-08 12:48:19 +0500

    replaced the code to see if it's working

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,75 @@
+const fss = require('fs');
+const path = require('path');
 const express = require('express');
-
+import {Request, Response} from 'express'
 const app = express();
 
-app.get('/', (req, res) => res.send("Express on vercell"));
+const PORT = 4000 || process.env.PORT;
 
-app.listen(3000, ()=> console.log('Server is ready on 3000'));
+app.get("/", async (req: Request, res: Response) => {
+    return res.send("Hello! Man.")
+})
 
-module.exports = app;
+app.get("/data", async (req: Request, res: Response) => {
+    // return res.text("Hello");
+    // res.send("Hellos");
+    // return res.json({msg:"Hello World"});
+    // readData()
+    try {
+        const dt = await readData()
+        console.log('dtt', dt)
+        return res.json({data: dt});
+    } catch (err) {
+        console.error('Error reading data:', err);
+        return res.status(500).json({ error: 'Failed to read data' });
+    }
+})
+
+// app.listen(PORT, (req: Request, res: Response)=> {
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
+function saveData(turnIntoJsonData: string){
+    const filePath = './data.json';
+    // const filePath = path.join(__dirname, 'data.json');
+
+    fss.writeFile(filePath, JSON.stringify(turnIntoJsonData), 'utf8', (e:any) => {
+        if(e){
+            console.error('Error writing to file:', e);
+        }else {
+            console.log("Data Successfully saved")
+        }
+    })
+}
+
+function readData(): Promise<any>{
+    // const filePath = path.join(__dirname, 'data.json');
+    const filePath = './data.json';
+
+    return new Promise((resolve, reject) => {
+        fss.readFile(filePath, 'utf8', (e:any, data: string) => {
+        if(e){
+            console.error('Error writing to file:', e);
+            reject(e);
+        }else {
+            // const parsedData = JSON.parse(data);
+            // console.log("Data Successfully retrieved", parsedData)
+            // return parsedData;
+            try {
+                const parsedData = JSON.parse(data);
+                console.log("Data successfully retrieved", parsedData);
+                resolve(parsedData);
+            } catch (parseErr) {
+                reject(parseErr);
+            }
+        }
+    })
+})
+}
+
+// saveData("Hello")
+
+readData()
+
+module.exports = app
